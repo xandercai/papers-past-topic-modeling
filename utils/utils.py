28363857306770b302f882e3ae52755125eeba9e
xandercai <xandcai@gmail.com>
2019-01-26 22:45:47 +1300

    init

--- utils/utils.py
+++ utils/utils.py
@@ -13,7 +13,10 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
 from pyspark.sql.session import SparkSession
-
+from matplotlib import pyplot as plt
+plt.style.use('ggplot')
+import seaborn as sns
+sns.axes_style("darkgrid")
 
 
 #https://stackoverflow.com/questions/2104080/how-to-check-file-size-in-python
@@ -163,3 +166,47 @@ def load_dataset(dataset, spark):
         df = -1
 
     return df
+
+
+
+# for plot:
+
+def filter_topics(df, topic_list):
+    # remove data out of df if topic is not in topic_list
+    return (df[df['topic'].isin(topic_list)])
+
+def filter_regions(df, region_list):
+    # remove data out of df if topic is not in topic_list
+    return (df[df['region'].isin(region_list)])
+
+
+def plot_topics(df, kind='', col_order=None, adjust_top=0.97, title=None, height=3.5, col_wrap=2):
+    if kind == 'scatter':
+        g = sns.catplot(x="year", y='weight', hue="topic",
+                        col='keywords', col_wrap=col_wrap, col_order=col_order,
+                        kind='strip', height=height, aspect=2, jitter=1, dodge=False, legend=False,
+                        s=4, alpha=0.6, edgecolors='w',
+                        data=df)
+        g.fig.suptitle("Dominant Topics Distribution of {}".format(title), fontsize=16)
+
+    elif kind == 'bar':
+        g = sns.catplot(x="year", hue="topic",
+                        col='keywords', col_wrap=col_wrap, col_order=col_order,
+                        kind='count', height=height, aspect=2, dodge=False, legend=False,
+                        data=df)
+        g.fig.suptitle("Dominant Topics Count of {}".format(title), fontsize=16)
+
+    elif kind == 'point':
+        g = sns.catplot(x="year", y='weight',
+                        col='keywords', col_wrap=col_wrap, col_order=col_order,
+                        kind='point', height=height, aspect=2, dodge=False, s=1, legend=False,
+                        markers='.', scale=0.5,
+                        data=df)
+        g.fig.suptitle("Average Weight of {}".format(title), fontsize=16)
+
+    else:
+        print('wrong kind.')
+
+    g.fig.subplots_adjust(top=adjust_top)
+    g.set_xticklabels(rotation=90, step=2)
+    return g
